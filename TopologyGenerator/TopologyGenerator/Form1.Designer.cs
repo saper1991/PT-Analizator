78f6e302b93ebdea71c0fdd39ae329b6774abe25
leaswis <swislaura@gmail.com>
2015-05-26 18:37:03 +0200

    zmiana form1

--- TopologyGenerator/TopologyGenerator/Form1.Designer.cs
+++ TopologyGenerator/TopologyGenerator/Form1.Designer.cs
@@ -38,22 +38,28 @@
             // 
             // DeleteFileButton
             // 
+            this.DeleteFileButton.BackColor = System.Drawing.Color.White;
+            this.DeleteFileButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.DeleteFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.DeleteFileButton.Location = new System.Drawing.Point(24, 175);
             this.DeleteFileButton.Name = "DeleteFileButton";
             this.DeleteFileButton.Size = new System.Drawing.Size(144, 36);
             this.DeleteFileButton.TabIndex = 2;
             this.DeleteFileButton.Text = "Usuń plik";
-            this.DeleteFileButton.UseVisualStyleBackColor = true;
+            this.DeleteFileButton.UseVisualStyleBackColor = false;
             this.DeleteFileButton.Click += new System.EventHandler(this.DeleteFileButton_Click);
             // 
             // LoadFileButton
             // 
+            this.LoadFileButton.BackColor = System.Drawing.Color.White;
+            this.LoadFileButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.LoadFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoadFileButton.Location = new System.Drawing.Point(24, 12);
             this.LoadFileButton.Name = "LoadFileButton";
             this.LoadFileButton.Size = new System.Drawing.Size(144, 36);
             this.LoadFileButton.TabIndex = 1;
             this.LoadFileButton.Text = "Wczytaj dane";
-            this.LoadFileButton.UseVisualStyleBackColor = true;
+            this.LoadFileButton.UseVisualStyleBackColor = false;
             this.LoadFileButton.Click += new System.EventHandler(this.LoadFileButton_Click);
             // 
             // listOfFilesListBox
@@ -66,38 +72,48 @@
             // 
             // GenerateButton
             // 
+            this.GenerateButton.BackColor = System.Drawing.Color.White;
+            this.GenerateButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.GenerateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.GenerateButton.Location = new System.Drawing.Point(24, 73);
             this.GenerateButton.Name = "GenerateButton";
             this.GenerateButton.Size = new System.Drawing.Size(144, 36);
             this.GenerateButton.TabIndex = 3;
             this.GenerateButton.Text = "Generuj";
-            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.UseVisualStyleBackColor = false;
             this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
             // 
             // ExitButton
             // 
+            this.ExitButton.BackColor = System.Drawing.Color.White;
+            this.ExitButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ExitButton.Location = new System.Drawing.Point(24, 295);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(144, 36);
             this.ExitButton.TabIndex = 4;
             this.ExitButton.Text = "Zamknij program";
-            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.UseVisualStyleBackColor = false;
             this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // clearAllButton
             // 
+            this.clearAllButton.BackColor = System.Drawing.Color.White;
+            this.clearAllButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.clearAllButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.clearAllButton.Location = new System.Drawing.Point(24, 236);
             this.clearAllButton.Name = "clearAllButton";
             this.clearAllButton.Size = new System.Drawing.Size(144, 36);
             this.clearAllButton.TabIndex = 5;
             this.clearAllButton.Text = "Wyczyść wszystko";
-            this.clearAllButton.UseVisualStyleBackColor = true;
+            this.clearAllButton.UseVisualStyleBackColor = false;
             this.clearAllButton.Click += new System.EventHandler(this.clearAllButton_Click);
             // 
             // MainWnd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(487, 363);
             this.ControlBox = false;
             this.Controls.Add(this.clearAllButton);
